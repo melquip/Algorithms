c7677507083f20659be275e61be11fbd172b97c2
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-19 16:37:53 +0000

    Completed `eating_cookies` with memoization

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,37 +6,24 @@ import functools
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+def memoize(f):
+  cache = dict()
+  def memoized(n, c = cache):
+    if n in c:
+      return cache[n]
+    result = f(n, c)
+    cache[n] = result
+    return result
+  return memoized
 
-def eating_cookies(n, cache=None):
-  if n < 1:
+@memoize
+def eating_cookies(n, cache = {}):
+  if n < 0:
+    return 0
+  elif n <= 1:
     return 1
-  actions = [1, 2, 3]
-  currCookie = n
-  allCombs = [[action] for action in actions if action <= n]
-  # print('init?', responseArr)
-  while (currCookie > 1):
-    combs = []
-    for action in actions:
-      combinations = []
-      for resArr in allCombs:
-        newCombination = None
-        if sum(resArr) + action <= n:
-          newCombination = [action] + resArr
-        elif sum(resArr) == n:
-          newCombination = resArr
-        # print('resArr, action, newCombination', resArr, action, newCombination)
-        if newCombination != None:
-          combinations.append(newCombination)
-      # print('combinations', combinations)
-      combs += combinations
-    allCombs = combs
-    currCookie -= 1
-
-  # remove duplicates in place
-  allCombs = [allCombs[i] for i in range(0, len(allCombs)) if allCombs.index(allCombs[i]) == i]
-  print('response:', allCombs)
-  return len(allCombs)
-  
+  else:
+    return eating_cookies(n - 3, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 1, cache)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
