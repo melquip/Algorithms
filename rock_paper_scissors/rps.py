73553313d386091f8ad45c00e6dc21151e74cfa3
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-20 15:09:33 +0000

    Completed `rps.py` exercise

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,39 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+def memoize(f):
+  cache = {}
+  def memoized(n, r, c = {}):
+    c = cache
+    key = r + [n]
+    key = "".join(r)
+    if key in c:
+      return cache[key]
+    result = f(n, r, c)
+    cache[key] = result
+    return result
+  return memoized
+
+# (1) [['rock'], ['paper'], ['scissors']]
+# (2) [['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'], ['paper', 'rock'], ['paper', 'paper'], ['paper', 'scissors'], ['scissors', 'rock'], ['scissors', 'paper'], ['scissors', 'scissors']]
+
+def rock_paper_scissors(rounds):
+  outcomes = []
+  plays = ['rock', 'paper', 'scissors']
+
+  @memoize
+  def roundOutcome(roundsLeft, result, cache = {}):
+    if roundsLeft == 0:
+      outcomes.append(result)
+      return False
+    
+    for play in plays:
+      roundOutcome(roundsLeft - 1, result + [play])
+
+  roundOutcome(rounds, [])
+
+  return outcomes
+    
 
 
 if __name__ == "__main__":
