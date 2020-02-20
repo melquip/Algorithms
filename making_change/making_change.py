4b28a9e819ea968af8abd4349b8eb25fb3c23152
Melquisedeque Pereira <melqui1@live.com.pt>
2020-02-20 16:16:33 +0000

    Completed `making_change` exercise

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,9 +2,31 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
 
+def making_change(amount, coins = [1, 5, 10, 25, 50], cache = {}):
+  # get the all possible coins left
+  uniqueCoins = len(coins)
+  # coin value was higher than amount
+  # so dont add anything to total
+  if amount < 0: return 0
+  # finished coin combination
+  # so add 1 to total ways of making change for n $
+  elif amount == 0: return 1
+  # use cache to improve performance
+  elif (amount, uniqueCoins) in cache: return cache[(amount, uniqueCoins)]
+  # recursive loop
+  else:
+    total = 0
+    for c in range(0, uniqueCoins):
+      coin = coins[c]
+      if coin > amount:
+        continue
+      else:
+        result = making_change(amount - coin, coins[c:], cache)
+        if result > 0:
+          total += result
+    cache[(amount, uniqueCoins)] = total
+    return total
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
